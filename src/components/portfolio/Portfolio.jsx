c7ab9d45baa253f9c3ceebb493e114005b3ae94e
JaqiKal <jaqika@gmail.com>
2025-02-24 11:44:29 +0100

    feat: Add placeholder for Portfolio component

---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,15 @@
+/** <components/portfolio/Portfoli.jsx>
+ * This file contains the About component.
+ */
+const Portfolio = () => {
+  return (
+    <section
+      id="portfolio"
+      className="p-10 bg-secondary-500"
+    >
+      Browse through my work
+    </section>
+  );
+};
+
+export default Portfolio;
